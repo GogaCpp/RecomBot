c29d61281e31b61d1c077697fec35fef8479cadf
GogaCpp <assdddffff54@mail.ru>
2025-01-09 18:42:52 +0300

    ref text_to_metr

--- RecBot/scripts/text_to_metrs.py
+++ RecBot/scripts/text_to_metrs.py
@@ -1,19 +1,46 @@
 import re
+from ru_word2number import w2n
 
 
 def convert_to_meters(distance_str):
+    distance_str = distance_str.strip().lower()
 
     km_to_meters = 1000
     meters_to_meters = 1
 
-    distance_str = distance_str.strip().lower()
+    distance_value = None
 
-    match = re.search(r'(\d+(\.\d+)?)', distance_str)
-    if match:
-        distance_value = float(match.group(0))
+    for unit in ['км', 'километр', 'метров', 'м', 'метр']:
+        distance_str_clean = distance_str.replace(unit, '')
 
-    if 'км' in distance_str or 'километр' in distance_str:
-        return int(distance_value * km_to_meters)
-    elif 'м' in distance_str or 'метр' in distance_str:
-        return int(distance_value * meters_to_meters)
+    try:
+        distance_value = w2n.word_to_num(distance_str_clean.strip())
+    except ValueError:
+        match = re.search(r'(\d+(\.\d+)?)', distance_str)
+        if match:
+            distance_value = float(match.group(0))
+
+    if distance_value is not None:
+        original_distance_str = distance_str.strip()
+        if 'км' in original_distance_str or 'километр' in original_distance_str:
+            return int(distance_value * km_to_meters)
+        elif 'м' in original_distance_str or 'метр' in original_distance_str:
+            return int(distance_value * meters_to_meters)
+        else:
+            return distance_value
     return None
+
+
+if __name__ == "__main__":
+    print(convert_to_meters("1 км"))
+    print(convert_to_meters("1км"))
+    print(convert_to_meters("10000 метров"))
+    print(convert_to_meters("5 километров"))
+    print(convert_to_meters("1000 МЕТРОВ"))
+    print(convert_to_meters("1.5 КИЛОМЕТРА"))
+    print(convert_to_meters("один километр"))
+    print(convert_to_meters("двести метров"))
+    print(convert_to_meters("два километра"))
+    print(convert_to_meters("А если 1.5 км"))
+    print(convert_to_meters("А ЕСЛИ 2.28КИЛОМЕТРОВЫХ ПОБЕГУШЕК СЫНОК!!!"))
+    print(convert_to_meters("1000"))
